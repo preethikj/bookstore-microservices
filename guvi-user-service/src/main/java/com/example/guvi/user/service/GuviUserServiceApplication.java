5adc2999550adb9f5903f6291862ca9135897be3
preethikj <kjpreethi89@gmail.com>
2026-06-11 16:40:39 +0530

    Added User service

---
+++ guvi-user-service/src/main/java/com/example/guvi/user/service/GuviUserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.guvi.user.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuviUserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuviUserServiceApplication.class, args);
+	}
+
+}
